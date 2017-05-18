4eaa2114193290745f43594614c939998dcefc16
Julien Birgand <julien.birgand@altran.com>
2017-05-18 16:29:23 +0200

    Fix 'Google Maps API warning: RetiredVersion', use latest version

--- client/app/app.js
+++ client/app/app.js
@@ -3,7 +3,7 @@ angular
   .config(function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
       key: 'AIzaSyA6gr3zDRveuD-4BrFq5VzKcNS6bTRVofo',
-      v: '3.25', //defaults to latest 3.X anyhow
+      v: '3', //defaults to latest 3.X anyhow
       libraries: 'weather,geometry,visualization'
     });
   })
